42509508bf93b17a92b72dfd9246d4b337435b4f
Salah Saleh <salah@bernstein.io>
2019-09-19 20:57:28 +0200

    add person model

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Person, type: :model do
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:birthday) }
+end
