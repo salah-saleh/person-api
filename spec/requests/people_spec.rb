de37e0fa19e11fa941f2f3723a9585a1e5fcee39
Salah Saleh <salah@bernstein.io>
2019-09-19 22:45:05 +0200

    fix issue in the rpec index route

--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe 'People API', type: :request do
   # initialize test data 
   # let!(:people) { create_list(:person, 10) }
-  let(:people) { 10.times.map { |i| Person.create!(name: "potluck", hobbies: "football bla", birthday: "Oct 30th 2017") } }
+  let!(:people) { 10.times.map { |i| Person.create!(name: "potluck", hobbies: "football bla", birthday: "Oct 30th 2017") } }
   let(:person_id) { people.first.id }
 
   # Test suite for GET /people
@@ -14,7 +14,7 @@ RSpec.describe 'People API', type: :request do
 
     it 'returns people' do
       # Note `json` is a custom helper to parse JSON responses
-      # expect(json).not_to be_empty
+      expect(json).not_to be_empty
       expect(json.size).to eq(10)
     end
 
@@ -31,6 +31,7 @@ RSpec.describe 'People API', type: :request do
       it 'returns the person' do
         expect(json).not_to be_empty
         expect(json['id']).to eq(person_id)
+        expect(json['name']).to eq("potluck")
       end
 
       it 'returns status code 200' do
