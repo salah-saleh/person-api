34215d8dbf080b458394bfc2af9c9f35bcf2d0b9
Salah Saleh <salah.saleh.ali@gmail.com>
2019-09-22 12:59:04 +0200

    rollback user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_115155) do
+ActiveRecord::Schema.define(version: 2019_09_19_184256) do
 
   create_table "people", force: :cascade do |t|
     t.string "name"
@@ -18,17 +18,6 @@ ActiveRecord::Schema.define(version: 2019_09_20_115155) do
     t.string "hobbies"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "email", null: false
-    t.string "password_digest"
-    t.string "role", default: "user", null: false
-    t.datetime "last_login"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email"
   end
 
 end
